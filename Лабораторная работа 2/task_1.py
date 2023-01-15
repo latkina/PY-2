9a91653fb15b73283131c5b731b8fbe550441864
latkina <115399169+latkina@users.noreply.github.com>
2023-01-15 19:18:07 +0300

    Лабораторная работа 2

---
+++ Лабораторная работа 2/task_1.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, conint
+
+
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book(BaseModel):
+    """Класс, описывающий книгу"""
+    id_: int
+    name: str
+    pages: conint(gt=0)
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f'Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})'
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
