47ae376408fd5ff2eda56d87f7762caf2b6a124f
latkina <115399169+latkina@users.noreply.github.com>
2023-01-28 13:33:01 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,68 @@
+class Book:
+    """ Базовый класс книги. """
+
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    def __str__(self):
+        return f"Книга: {self.name}. Автор: {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    def __str__(self):
+        return f"Книга: {self.name}. Автор: {self.author}. Количество страниц: {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        if not isinstance(pages, int):
+            raise TypeError
+        if not pages > 0:
+            raise ValueError
+        self._pages = pages
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: (int, float)):
+        super().__init__(name, author)
+        self._duration = duration
+
+    def __str__(self):
+        return f"Книга: {self.name}. Автор: {self.author}. Продолжительность: {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: (int, float)) -> None:
+        if not isinstance(duration, (int, float)):
+            raise TypeError
+        if not duration > 0:
+            raise ValueError
+        self._duration = duration
